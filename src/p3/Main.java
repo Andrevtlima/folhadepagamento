1bf5ef15adc41e569820feb4d577ae67eaddd1ab
nees <andreducap@gmail.com>
2017-01-18 08:05:56 -0300

    Create Emp

--- src/p3/Main.java
+++ src/p3/Main.java
@@ -3,15 +3,34 @@ package p3;
 import java.sql.Date;
 
 public class Main {
+	public int id;
 
 	public static void main(String[] args) {
 		
-		Assalariados trabalhador = new Assalariados();
-		trabalhador.comissionado = true;
-		trabalhador.venda.data = Date.valueOf("2001-02-01");
-		trabalhador.venda.valor = 662.5;
+		
 		
 		
 	}
 
+	public static Trabalhador create_emp(String[] name, String[] end, short type, double sal, double comissao){
+		Trabalhador trabalhador = new Trabalhador();
+		trabalhador.name = name;
+		trabalhador.end = end;
+		trabalhador.type = type;
+		switch (type) {
+		case 1:
+			trabalhador.salario_horario = sal;
+			break;
+		case 2:
+			trabalhador.salario_mensal = sal;
+			break;	
+		case 3:
+			trabalhador.comissao = sal;
+			break;	
+		default:
+			break;
+		}
+		return null;
+		
+	}
 }
---
+++ src/p3/Trabalhador.java
@@ -0,0 +1,14 @@
+package p3;
+
+public class Trabalhador {
+	short type;
+	String[] name;
+	String[] end;
+	int percentual_comissao;
+	int horas_trabalhadas;
+	int id;
+	double salario_horario;
+	double salario_mensal;
+	double comissao;
+	VENDA[] vendas;	
+}
